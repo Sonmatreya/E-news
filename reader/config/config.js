d7558e47c67d3404b381c130671a71922a8bf8ec
Sonmatreya <baug.sonmatreya@gmail.com>
2026-02-11 16:37:24 +0530

    Modified

--- reader/config/config.js
+++ reader/config/config.js
@@ -1,18 +1,6 @@
-const production = 'production'
-const local = 'dev'
+const base_api_url =
+  process.env.NODE_ENV === "production"
+    ? "https://e-news-dkp7.onrender.com"
+    : "http://localhost:5000";
 
-const local_api_url = 'http://localhost:5000'
-
-const production_api_url = 'https://e-news-dkp7.onrender.com'
-
-const mode = production  
-
-let base_api_url = ''
-
-if (mode === production) {
-    base_api_url = production_api_url
-} else {
-    base_api_url = local_api_url
-}
-
-export { base_api_url }
+export { base_api_url };
