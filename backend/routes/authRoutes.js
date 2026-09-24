2d9e04ada02ebbc231f92a5e7d5d8c4b9e7f56ca
Sonmatreya Baug <baug.sonmatreya@gmail.com>
2026-09-24 15:07:55 +0530

    Add profile image update route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -11,5 +11,6 @@ router.get('/api/news/staff',middleware.auth,middleware.role, authController.get
 router.delete('/api/news/member/:id',middleware.auth,middleware.role, authController.delete_member)
 router.get('/api/news/writer/:id',middleware.auth,middleware.role, authController.get_writer)
 router.post('/api/change-password', middleware.auth, authController.change_password)
+router.post('/api/update-profile-image', middleware.auth, authController.update_profile_image)
 
 module.exports = router
