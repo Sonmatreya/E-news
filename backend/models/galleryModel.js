702022e19b2c861987053ee0620610b46a1f165e
Sonmatreya Baug <baug.sonmatreya@gmail.com>
2026-09-25 12:44:28 +0530

    Fix gallery model for photographer image workflow

--- backend/models/galleryModel.js
+++ backend/models/galleryModel.js
@@ -1,14 +1,28 @@
 const { model, Schema } = require('mongoose')
 
 const galler_schema = new Schema({
-    writerId: {
+    photographerId: {
         type: Schema.Types.ObjectId,
         required: true,
         ref: 'authors'
     },
+    photographerName: {
+        type: String,
+        required: true,
+        default: ''
+    },
+    caption: {
+        type: String,
+        default: ''
+    },
     url: {
         type: String,
         required: true
+    },
+    status: {
+        type: String,
+        enum: ['available', 'used'],
+        default: 'available'
     }
 }, { timestamps: true })
 
